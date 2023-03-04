09d6b552b279ec94590e0d3ba48ccc4bb77c4a38
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-04 16:17:30 +0800

    update routing and return html

--- crash-course/server.js
+++ crash-course/server.js
@@ -1,9 +1,46 @@
 const http = require('http')
+const fs = require('fs')
 
 const server = http.createServer((req, res)=>{
-        console.log('request added')
+    // request   
+    console.log(req.url, req.method)
+
+    // response (basic)
+    // res.setHeader('Content-Type', 'text/html')
+    
+    // res.write('<p>Heloo</p>')
+
+    // res.end()
+
+
+
+    // return HTML pages
+    res.setHeader('Content-Type', 'text/html')
+
+// routing
+    let path = './views/'
+    switch (req.url) {
+        case '/':
+            path+='index.html'
+            break;
+        case '/about':
+            path+='about.html'
+            break;
+        default: path+='404.html'
+            break;
+    }
+     
+    fs.readFile(path, (err,data)=>{
+        if (err) throw err   
+
+        //res.write(data)
+        res.end(data)
+    })
+
 })
 
+
+//listen to request
 server.listen(3000, 'localhost', ()=>{
     console.log('listen to port 3000')
 })
