6d0bd9de9ff5763bd16ba62f4dfcf2f946ea3491
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-04 14:26:57 +0800

    clear client

--- crash-course/modules.js
+++
@@ -1,8 +0,0 @@
-const {people,ages} = require('./people')
-
-console.log(people, ages)// return empty object
-
-const os = require('os') // built-in modules in js (one of them is os)
-// console.log(os) //print os spec for the system
-
-console.log(os.platform(), os.homedir()) 
--- crash-course/people.js
+++
@@ -1,6 +0,0 @@
-const people = ['yoshi','mario', 'jamal', 'kamarul']
-const ages = [20, 25, 30, 35]
-
-console.log(people)
-
-module.exports = {people, ages}
