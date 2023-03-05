ba03245fc064d10cb3ae1cd1ca1e3943aabb6239
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-05 21:27:52 +0800

    express

--- crash-course/app.js
+++ crash-course/app.js
@@ -7,15 +7,24 @@ const app = express();
 app.listen(3000);
 
 app.get('/', (req,res)=>{
-    res.send('HAYAI')
+    // res.send('HAYAI')
+    res.sendFile('./views/index.html' , {root: __dirname})
+})
+
+app.get('/about', (req,res)=>{
+    // res.send('About HAYAI')
+    res.sendFile('./views/about.html' , {root: __dirname})
 })
 
 
+// redirect
+app.get('/about-us' , (req,res)=>{
+    res.redirect('/about')
+})
 
-
-
-
-
-
+// 404
+app.use((req,res)=>{
+    res.status(404).sendFile('./views/404.html' , {root: __dirname})
+})
 
 
