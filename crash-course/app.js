223b21e2e231e63449eea7935d5912119c29ff77
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-05 16:09:13 +0800

    Update app.js

--- crash-course/app.js
+++ crash-course/app.js
@@ -1,2 +1,21 @@
 const express = require('express')
 
+// express app
+const app = express();
+
+// listen for request 
+app.listen(3000);
+
+app.get('/', (req,res)=>{
+    res.send('HAYAI')
+})
+
+
+
+
+
+
+
+
+
+
