4d10d09e7fe89a674ec0857aed870d17cd1ed157
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-06 12:52:23 +0800

    ejs views & rendering

--- crash-course/app.js
+++ crash-course/app.js
@@ -11,23 +11,21 @@ app.listen(3000);
 
 app.get('/', (req,res)=>{
     // res.send('HAYAI')
-    res.sendFile('./views/index.html' , {root: __dirname})
+    res.render('index') 
 })
 
 app.get('/about', (req,res)=>{
     // res.send('About HAYAI')
-    res.sendFile('./views/about.html' , {root: __dirname})
+    res.render('about')
 })
 
-
-// redirect
-app.get('/about-us' , (req,res)=>{
-    res.redirect('/about')
+app.get('/blogs/create', (req,res) => {
+    res.render('create')
 })
 
 // 404
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html' , {root: __dirname})
+    res.status(404).render('404')
 })
 
 
