292bc513c27f4a9fe2cce0057dc12fc198ebe297
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-06 11:35:01 +0800

    Update server.js

--- crash-course/server.js
+++ crash-course/server.js
@@ -1,65 +1,65 @@
-// const http = require('http')
-// const fs = require('fs')
-// const _ = require('lodash')
+const http = require('http')
+const fs = require('fs')
+const _ = require('lodash')
 
-// const server = http.createServer((req, res)=>{
-//     // request   
-//    // console.log(req.url, req.method)
+const server = http.createServer((req, res)=>{
+    // request   
+   // console.log(req.url, req.method)
 
-//     //lodash
-//     const num = _.random(0 , 20) // pick random num between 0 - 20
-//     console.log(num)
+    //lodash
+    const num = _.random(0 , 20) // pick random num between 0 - 20
+    console.log(num)
     
-//     const greet = _.once(()=>{ //run only once
-//         console.log('hello')
-//     })
+    const greet = _.once(()=>{ //run only once
+        console.log('hello')
+    })
 
-//     greet()
-//     greet()
+    greet()
+    greet()
 
-//     // response (basic)
-//     // res.setHeader('Content-Type', 'text/html')
+    // response (basic)
+    // res.setHeader('Content-Type', 'text/html')
     
-//     // res.write('<p>Heloo</p>')
+    // res.write('<p>Heloo</p>')
 
-//     // res.end()
+    // res.end()
 
 
 
-//     // return HTML pages
-//     res.setHeader('Content-Type', 'text/html')
+    // return HTML pages
+    res.setHeader('Content-Type', 'text/html')
 
-// // routing
-//     let path = './views/'
-//     switch (req.url) {
-//         case '/':
-//             path+='index.html'
-//             res.statusCode = 200
-//             break;
-//         case '/about':
-//             path+='about.html'
-//             res.statusCode = 200
-//             break;
-//         case '/about-me': //redirect (similar name but different file)
-//             res.statusCode = 301
-//             res.setHeader('Location', '/about')
-//             res.end()
-//         default: path+='404.html'
-//             res.statusCode = 404
-//             break;
-//     }
+// routing
+    let path = './views/'
+    switch (req.url) {
+        case '/':
+            path+='index.html'
+            res.statusCode = 200
+            break;
+        case '/about':
+            path+='about.html'
+            res.statusCode = 200
+            break;
+        case '/about-me': //redirect (similar name but different file)
+            res.statusCode = 301
+            res.setHeader('Location', '/about')
+            res.end()
+        default: path+='404.html'
+            res.statusCode = 404
+            break;
+    }
      
-//     fs.readFile(path, (err,data)=>{
-//         if (err) throw err   
+    fs.readFile(path, (err,data)=>{
+        if (err) throw err   
 
-//         //res.write(data)
-//         res.end(data)
-//     })
+        //res.write(data)
+        res.end(data)
+    })
 
-// })
+})
 
 
-// //listen to request
-// server.listen(3000, 'localhost', ()=>{
-//     console.log('listen to port 3000')
-// })
+//listen to request
+server.listen(3000, 'localhost', ()=>{
+    console.log('listen to port 3000')
+})
