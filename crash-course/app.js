c5c76d9c9d9e8d200cd5040ecd5e22c9793ea12d
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-09 11:43:16 +0800

    GET by id

--- crash-course/app.js
+++ crash-course/app.js
@@ -85,6 +85,19 @@ app.post('/blogs', (req,res) => {
     })
 })
 
+
+app.get('/blogs/:id',  (req,res) => {
+    const id = req.params.id //define id 
+
+    Blog.findById(id)
+        .then(result => {
+            res.render('details', { blog: result, title: 'Blog Details'})
+        })
+        .catch(err => {
+            console.log(err)
+        })
+})
+
 app.get('/blogs/create', (req,res) => {
     res.render('create', {title: 'create new blog'})
 })
