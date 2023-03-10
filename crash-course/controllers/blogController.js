c7de35fbceec5f594345ca78245189bb145910fe
Hayai <108877345+Ifansyfiq@users.noreply.github.com>
2023-03-10 11:19:26 +0800

    404 id not found

--- crash-course/controllers/blogController.js
+++ crash-course/controllers/blogController.js
@@ -42,7 +42,7 @@ const blog_create_post = (req,res) => {
             res.render('blogs/details', { blog: result, title: 'Blog Details'})
         })
         .catch(err => {
-            console.log(err)
+            res.status(404).render('404', {title: 'Blog not found'})
         })
 }
 
@@ -55,7 +55,7 @@ const blog_delete = (req,res) => {
             res.json({redirect: '/blogs'})
         })
         .catch(err => {
-            console.log(err)
+            res.status(404).render('404', {title: 'Blog not found'})
         })
 }
 
